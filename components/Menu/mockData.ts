f914bd059eb584c29c1bdc5a56e2ac93282a809f
Patryk Podworski <patryk.podworski@gmail.com>
2024-06-02 23:01:47 +0200

    Add taste notes and bullet list (#2)
    Co-authored-by: Patryk Podworski <patryk@xtramile.no>

--- components/Menu/mockData.ts
+++ components/Menu/mockData.ts
@@ -2,31 +2,37 @@ export const mockData: Cocktail[] = [
   {
     name: "Old Fashioned",
     ingredients: ["Bourbon", "Simple Syrup", "Angostura Aromatic Bitters"],
+    tasteNotes: "strong, smooth",
     src: "/cocktails/old-fashioned.png",
   },
   {
     name: "Daiquiri",
     ingredients: ["White Rum", "Lime Juice", "Simple Syrup"],
+    tasteNotes: "sweet, sour",
     src: "/cocktails/daiquiri.png",
   },
   {
     name: "Gin & Tonic",
     ingredients: ["Gin", "Tonic Water", "Lime"],
+    tasteNotes: "refreshing, bitter",
     src: "/cocktails/gin-tonic.png",
   },
   {
     name: "Negroni",
     ingredients: ["Gin", "Campari", "Sweet Vermouth"],
+    tasteNotes: "bitter, strong",
     src: "/cocktails/negroni.png",
   },
   {
     name: "French 75",
     ingredients: ["Gin", "Lemon Juice", "Simple Syrup", "Champagne"],
+    tasteNotes: "bubbly, light",
     src: "/cocktails/french-75.png",
   },
   {
     name: "Espresso Martini",
     ingredients: ["Vodka", "Kahlúa", "Espresso"],
+    tasteNotes: "energizing, decadent",
     src: "/cocktails/espresso-martini.png",
   },
 ];
@@ -34,5 +40,6 @@ export const mockData: Cocktail[] = [
 type Cocktail = {
   name: string;
   ingredients: string[];
+  tasteNotes: string;
   src: string;
 };
